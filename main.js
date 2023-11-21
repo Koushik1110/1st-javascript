e72cf3e1a9ef33fcc0c0791a65afc1cf72bfd46b
Koushik1110 <148125270+Koushik1110@users.noreply.github.com>
2023-11-21 17:40:27 +0600

    1st javascript

---
+++ main.js
@@ -0,0 +1,33 @@
+// console.log("1st problem")
+const morning = "feeding time";
+const afternoon = "feeding time";
+if (morning == "feeding time") {
+  console.log("It's time to feed the animals.");
+}
+else if (afternoon == "feeding time") {
+  console.log("Only be fed if morning feeding is missed");
+} else {
+  console.log("You can't feed the animals now.");
+}
+console.log(afternoon)
+
+// console.log("2nd problem")
+
+const maxWeight = 500;
+const passengerWeight = 480
+
+if(maxWeight<=passengerWeight){
+  console.log("The boat will cross the river without any problem.")
+} else {
+  console.log("The boat will sink")
+}
+
+
+// console.log("3rd problem")
+
+const myName = "Koushik";
+const favoriteHobby = "Reading books and watching anime";
+
+const sentence = `Hi, my name is ${myName}. My favorite hobby is ${favoriteHobby}.`;
+
+console.log(sentence)
