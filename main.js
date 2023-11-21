fcbc1defe8a255f70da1bf18b09c45b5f0755c80
Koushik1110 <148125270+Koushik1110@users.noreply.github.com>
2023-11-21 18:55:02 +0600

    finally got it

--- main.js
+++ main.js
@@ -1,25 +1,24 @@
 // console.log("1st problem")
-const morning = "feeding time";
-const afternoon = "feeding time";
-if (morning == "feeding time") {
+const feedTime = "night";
+
+if (feedTime == "morning") {
   console.log("It's time to feed the animals.");
 }
-else if (afternoon == "feeding time") {
+else if (feedTime == "afternoon") {
   console.log("Only be fed if morning feeding is missed");
 } else {
   console.log("You can't feed the animals now.");
 }
-console.log(afternoon)
 
 // console.log("2nd problem")
 
 const maxWeight = 500;
-const passengerWeight = 480
+const passengerWeight = 480;
 
-if(maxWeight<=passengerWeight){
-  console.log("The boat will cross the river without any problem.")
+if(maxWeight>=passengerWeight){
+  console.log("The boat will cross the river without any problem.");
 } else {
-  console.log("The boat will sink")
+  console.log("The boat will sink");
 }
 
 
